a1d4c81901cc11aa24ff30edcccec2e3c28bd034
hhhhector <hectordbahena@gmail.com>
2025-10-28 17:33:46 -0600

    test dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,14 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Event Elo")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
