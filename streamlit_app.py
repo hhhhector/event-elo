6a69b839d01e6bffab714731ee52a109f1cdfbee
hhhhector <hectordbahena@gmail.com>
2025-10-28 18:05:51 -0600

    restructure repo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,17 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 
 st.title("Event Elo")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+chart_data = pd.DataFrame(
+     np.random.randn(2000, 3),
+     columns=['a', 'b', 'c'])
 
-df
+st.line_chart(chart_data)
+
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
