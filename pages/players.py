4a048a55e94811ccf417c9da734c12dec6715dd4
hhhhector <hectordbahena@gmail.com>
2025-11-11 19:06:24 -0600

    rearrange players

--- pages/players.py
+++ pages/players.py
@@ -44,113 +44,116 @@ st.write(
     '```'
     )
 
-col1, col2 = st.columns(2)
+col1, col2 = st.columns([0.4, 0.6])
 
 with col1:
-    m = st.slider("Players", value = 10, min_value = 1, max_value = len(rankings))
+
+    col11, col12 = st.columns(2)
+
+    with col11:
+        m = st.slider("Players", value = 10, min_value = 1, max_value = len(rankings))
+
+    with col12:
+        n = st.slider("Events", value = rankings.shape[1] - 8, min_value = 1, max_value = rankings.shape[1] - 8)
+
+    for col in rankings.select_dtypes(include=['float']).columns:
+        rankings[col] = rankings[col].round(0).astype('int64')
+
+    plot_data_wide = rankings[rankings["Player"] == selected_player].reset_index().drop(
+        columns = ["Avatar", "Rating", "Peak", "Events", "I", "index"]
+    ).head(m).set_index("Player").T.iloc[-n:,:]
+    plot_data_wide.reset_index()
+    plot_data_wide['SortOrder'] = range(len(plot_data_wide))
+    plot_data_wide = plot_data_wide.reset_index().rename(columns={'index': 'Event'})
+    plot_data_long = plot_data_wide.melt(
+        id_vars=['Event', 'SortOrder'],  
+        var_name='Player',            
+        value_name='Rating'           
+    )
+
+
+    chart = alt.Chart(plot_data_long, height=600).mark_line(point=alt.OverlayMarkDef(size=100, opacity=0), size=1.5).encode(
+        
+        x=alt.X('Event', 
+            sort=alt.SortField('SortOrder')
+        ),
+        
+        y=alt.Y('Rating',
+            scale=alt.Scale(domain=[min(plot_data_long["Rating"]) - 100,max(plot_data_long["Rating"]) + 100])
+        ),
+
+        color=alt.value('#EAE151'),
+
+        tooltip=['Event', 'Rating']
+
+    ).interactive()
+
+    st.altair_chart(chart)
 
 with col2:
-    n = st.slider("Events", value = rankings.shape[1] - 8, min_value = 1, max_value = rankings.shape[1] - 8)
+    event_set = reversed(list(summaries.keys()))
 
-for col in rankings.select_dtypes(include=['float']).columns:
-    rankings[col] = rankings[col].round(0).astype('int64')
+    filtered_event_set = []
 
-plot_data_wide = rankings[rankings["Player"] == selected_player].reset_index().drop(
-    columns = ["Avatar", "Rating", "Peak", "Events", "I", "index"]
-).head(m).set_index("Player").T.iloc[-n:,:]
-plot_data_wide.reset_index()
-plot_data_wide['SortOrder'] = range(len(plot_data_wide))
-plot_data_wide = plot_data_wide.reset_index().rename(columns={'index': 'Event'})
-plot_data_long = plot_data_wide.melt(
-    id_vars=['Event', 'SortOrder'],  
-    var_name='Player',            
-    value_name='Rating'           
-)
-
-
-chart = alt.Chart(plot_data_long, height=600).mark_line(point=alt.OverlayMarkDef(size=100, opacity=0), size=1.5).encode(
-    
-    x=alt.X('Event', 
-        sort=alt.SortField('SortOrder')
-    ),
-    
-    y=alt.Y('Rating',
-        scale=alt.Scale(domain=[min(plot_data_long["Rating"]) - 100,max(plot_data_long["Rating"]) + 100])
-    ),
-
-    color=alt.value('#EAE151'),
-
-    tooltip=['Event', 'Rating']
-
-).interactive()
-
-
-st.altair_chart(chart)
-event_set = reversed(list(summaries.keys()))
-
-filtered_event_set = []
-
-filtered_event_set = [
-    event_name for event_name in event_set  
-    if (not selected_player or selected_player.lower() in [x.lower() for x in summaries[event_name]['players']])
-]
-
-player_events_df = pd.DataFrame(columns=['Event', 'Date', 'Position', 'Rating', 'Global', 'EP', 'AP', 'Score', 'New Rating', 'Rating Change', 'New Global', 'Global Change'])
-
-for event in filtered_event_set:
-    summary_results = summaries[event]['results']
-    player_events_df.loc[event] = [
-        event, 
-        summaries[event]['date'],
-        summary_results.loc[selected_id]['Position'],
-        summary_results.loc[selected_id]['Rating'],
-        summary_results.loc[selected_id]['Global'],
-        summary_results.loc[selected_id]['EP'],
-        summary_results.loc[selected_id]['AP'],
-        summary_results.loc[selected_id]['Score'],
-        summary_results.loc[selected_id]['New Rating'],
-        summary_results.loc[selected_id]['Rating Change'],
-        summary_results.loc[selected_id]['New Global'],
-        summary_results.loc[selected_id]['Global Change']
+    filtered_event_set = [
+        event_name for event_name in event_set  
+        if (not selected_player or selected_player.lower() in [x.lower() for x in summaries[event_name]['players']])
     ]
 
-for col in ['Rating', 'Rating Change', 'New Rating']:
-    player_events_df[col] = player_events_df[col].round(0).astype('int64')
+    player_events_df = pd.DataFrame(columns=['Event', 'Date', 'Position', 'Rating', 'Global', 'EP', 'AP', 'Score', 'New Rating', 'Rating Change', 'New Global', 'Global Change'])
 
-for col in ['EP', 'AP', 'Score']:
-    player_events_df[col] = player_events_df[col].round(1).astype('float')
+    for event in filtered_event_set:
+        summary_results = summaries[event]['results']
+        player_events_df.loc[event] = [
+            event, 
+            summaries[event]['date'],
+            summary_results.loc[selected_id]['Position'],
+            summary_results.loc[selected_id]['Rating'],
+            summary_results.loc[selected_id]['Global'],
+            summary_results.loc[selected_id]['EP'],
+            summary_results.loc[selected_id]['AP'],
+            summary_results.loc[selected_id]['Score'],
+            summary_results.loc[selected_id]['New Rating'],
+            summary_results.loc[selected_id]['Rating Change'],
+            summary_results.loc[selected_id]['New Global'],
+            summary_results.loc[selected_id]['Global Change']
+        ]
 
-rc_abs_max = player_events_df['Rating Change'].abs().max()/3
-gc_abs_max = player_events_df['Global Change'].abs().max()/3
+    for col in ['Rating', 'Rating Change', 'New Rating']:
+        player_events_df[col] = player_events_df[col].round(0).astype('int64')
 
-st.header('Played Events')
+    for col in ['EP', 'AP', 'Score']:
+        player_events_df[col] = player_events_df[col].round(1).astype('float')
 
-with st.container(horizontal_alignment='center'):
+    rc_abs_max = player_events_df['Rating Change'].abs().max()/3
+    gc_abs_max = player_events_df['Global Change'].abs().max()/3
 
-    st.dataframe(
-        player_events_df.style.text_gradient(
-                cmap='RdYlGn', subset=['Rating Change'],vmin=-rc_abs_max,vmax=rc_abs_max
-            ).text_gradient(
-                cmap='RdYlGn', subset=['Global Change'],vmin=-gc_abs_max,vmax=gc_abs_max
-            ).set_properties(
-                subset=['EP', 'AP', 'Score'],
-                **{'color':'dimgray'}
-            ), 
-        hide_index=True, 
-        width='content',
-        column_config={
-        'Position': st.column_config.NumberColumn(format='#%d'),
-        'Date': st.column_config.DateColumn(format = 'iso8601'),
-        'Rating': st.column_config.NumberColumn('Old Rating'),
-        'Global': st.column_config.NumberColumn("Old Global", format = "#%d"),
-        'EP': st.column_config.NumberColumn(format="%.1f"),
-        'AP': st.column_config.NumberColumn(format="%.1f"),
-        'Score': st.column_config.NumberColumn(format="%.1f"),
-        'Rating Change': st.column_config.NumberColumn("",format='%+.0f'),
-        'New Global': st.column_config.NumberColumn(format = "#%d"),
-        'Global Change': st.column_config.NumberColumn("",format='%+.0f'),
-        },
+    with st.container(horizontal_alignment='center'):
 
-    )
+        st.dataframe(
+            player_events_df.style.text_gradient(
+                    cmap='RdYlGn', subset=['Rating Change'],vmin=-rc_abs_max,vmax=rc_abs_max
+                ).text_gradient(
+                    cmap='RdYlGn', subset=['Global Change'],vmin=-gc_abs_max,vmax=gc_abs_max
+                ).set_properties(
+                    subset=['EP', 'AP', 'Score'],
+                    **{'color':'dimgray'}
+                ), 
+            hide_index=True, 
+            width='content',
+            column_config={
+            'Position': st.column_config.NumberColumn(format='#%d'),
+            'Date': st.column_config.DateColumn(format = 'iso8601'),
+            'Rating': st.column_config.NumberColumn('Old Rating'),
+            'Global': st.column_config.NumberColumn("Old Global", format = "#%d"),
+            'EP': st.column_config.NumberColumn(format="%.1f"),
+            'AP': st.column_config.NumberColumn(format="%.1f"),
+            'Score': st.column_config.NumberColumn(format="%.1f"),
+            'Rating Change': st.column_config.NumberColumn("",format='%+.0f'),
+            'New Global': st.column_config.NumberColumn(format = "#%d"),
+            'Global Change': st.column_config.NumberColumn("",format='%+.0f'),
+            },
+
+        )
         
 st.query_params["id"] = selected_player
